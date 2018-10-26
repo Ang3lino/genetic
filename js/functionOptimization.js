55028925179e46468ec0413de0c1924f2ba2357d
root <root@localhost.localdomain>
2018-10-26 02:40:39 +0000

    v:

--- js/functionOptimization.js
+++ js/functionOptimization.js
@@ -1,4 +1,20 @@
 
-nPoblations = 10;
-nIndividuals = 10;
-nBits = 5;
+variableCount = 2;
+restrictionCount = variablesCount;
+poblationCount = 10;
+individualCount = 10;
+bitsCount = 2;
+
+objectiveFunction = (x, y) => return x + y;
+
+restrictions = new Matrix(2, 2);
+
+// restrictions for the first variable
+restrictions.setElement(0, 0, 0);
+restrictions.setElement(0, 1, 0.5);
+
+// restrictions for the second variable
+restrictions.setElement(1, 0, 0);
+restrictions.setElement(1, 1, 1);
+
+
