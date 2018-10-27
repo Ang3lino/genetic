aa5feda6545b2140cb90991dba0eb0616559e4b0
Angel Lopez Manriquez <sigma271@outlook.com>
2018-10-27 01:17:20 +0000

    Add files via upload

--- js/functionOptimization.js
+++ js/functionOptimization.js
@@ -9,6 +9,20 @@ function generateVectors(bits) {
     return vectors;
 }
 
+function createRestrictions(amountVariables) {
+    restrictions = new Matrix(variableCount, 2);
+
+    // restrictions for the first variable
+    restrictions.setElement(0, 0, 0);
+    restrictions.setElement(0, 1, 0.5);
+
+    // restrictions for the second variable
+    restrictions.setElement(1, 0, 0);
+    restrictions.setElement(1, 1, 1);
+
+    return restrictions;
+}
+
 // Problem inicialization
 variableCount = 2;
 restrictionCount = variableCount;
@@ -16,18 +30,9 @@ poblationCount = 10;
 individualCount = 10;
 bitsCount = 1;
 
+restrictions = createRestrictions(variableCount);
+
 objectiveFunction = (x, y) => x + y;
-
-restrictions = new Matrix(2, 2);
-
-// restrictions for the first variable
-restrictions.setElement(0, 0, 0);
-restrictions.setElement(0, 1, 0.5);
-
-// restrictions for the second variable
-restrictions.setElement(1, 0, 0);
-restrictions.setElement(1, 1, 1);
-
 
 bitsRequired = [ ];
 for (var i = 0; i < variableCount; ++i) {
@@ -40,4 +45,38 @@ vectors = generateVectors(bitsRequired);
 console.log(bitsRequired);
 console.log(vectors);
 console.log(2 ** 7 - 1);
+
+function accumulateSums(numberArray) {
+    accumulate = [];
+    sum = 0;
+    numberArray.forEach( function (e) {
+        sum += e;
+        accumulate.push(sum);
+    });
+    return accumulate;
+}
+
+/**
+ *        4       12         8
+ * v =  1010 101010010110 1110000 
+ * x = (v >> (12 + 8)) & (2 ** 4 - 1)
+ * y = (v >> 8) & (2 ** 12 - 1)
+ * z = (v >> 0) & (2 ** 8 - 1)
+ * 
+ * sums = [8, 20, 24]
+ */
+function obtainValidVariables(vectors, bitsRequired, nIndividuals, nVariables, 
+        restrictions) {
+    validVariables = new Matrix(nIndividuals, nVariables);
+    sums = accumulateSums(bitsRequired.reverse());
+    shifts = [] ;
+    for (var i = 0; i < nVariables; ++i) {
+    }    
+    return validVariables;
+}
+
+validVariables = obtainValidVariables(vectors, bitsRequired, individualCount, 
+    variableCount, restrictions); 
+
+
 
